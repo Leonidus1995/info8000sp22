bf57c2d4eafe380947e9847b566dbe90e415121a
Gurjit Singh <gurjitsinghu0@gmail.com>
2022-02-01 11:41:17 -0500

    modified test.py

--- test.py
+++ test.py
@@ -6,10 +6,13 @@ f = open("test.txt")
 print(f)
 lines = f.readlines()
 print(lines)
+lines = [x.strip() for x in lines]
+print(lines)
 
 sum_1 = 0
 for item in lines:
-    sum_1 = sum_1 + int(item)
-    print("The sum is:", sum_1)
+    sum_1 = sum_1 + int(item.strip())
+    
+print(f"The sum is {sum_1}")
 
 print("The sum is also:", sum([int(x) for x in lines]))
